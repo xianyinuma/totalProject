32ce24d55d0e6ccc2ca26cdf321182d82d8afb78
濡尘赏海 <996956441@qq.com>
2017-05-07 15:10:18 +0800

    optimum

--- public/script/wenjin/chatroom.js
+++ public/script/wenjin/chatroom.js
@@ -21,7 +21,7 @@ function startChatRoom(currentPlayerName) {
   var connected = false;
   var typing = false;
   var lastTypingTime;
-  var $currentInput = $usernameInput.focus();
+  var $currentInput = $inputMessage.focus();
 
   var socket = io();
 
@@ -194,20 +194,27 @@ function startChatRoom(currentPlayerName) {
   // Keyboard events
 
   $window.keydown(function (event) {
-    // Auto-focus the current input when a key is typed
-    if (!(event.ctrlKey || event.metaKey || event.altKey)) {
-      $currentInput.focus();
-    }
-    // When the client hits ENTER on their keyboard
-    if (event.which === 13) {
-      if (username) {
-        sendMessage();
-        //socket.emit('stop typing');
-        typing = false;
-      } else {
-        setUsername();
+    // // Auto-focus the current input when a key is typed
+    // if (!(event.ctrlKey || event.metaKey || event.altKey)) {
+    //   $currentInput.focus();
+    // }
+    // // When the client hits ENTER on their keyboard
+    // if (event.which === 13) {
+    //   if (username) {
+    //     sendMessage();
+    //     //socket.emit('stop typing');
+    //     typing = false;
+    //   } else {
+    //     setUsername();
+    //   }
+    // }
+      if (event.which === 13 && $currentInput.is(":focus")) {
+        if (username) {
+          sendMessage();
+          socket.emit('stop typing');
+          typing = false;
+        }
       }
-    }
   });
 
   $inputMessage.on('input', function() {
