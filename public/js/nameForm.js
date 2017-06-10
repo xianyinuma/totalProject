94aad533dadfd57efd1f8cc588f5b7c1fbbc16a5
吴豪奇 <1427104424@qq.com>
2017-06-10 16:23:43 +0800

    用户名限制js更改

--- public/js/nameForm.js
+++ public/js/nameForm.js
@@ -1,7 +1,7 @@
 function confirm(){
     var username = document.getElementById("username");
     var string = username.value;
-    var head = string.substring(0,3);
+    var head = string.substring(0,3).toLowerCase();
     if(head=="npc"){
         alert("Oops!You cannot have a username start with npc!");
         return false;
